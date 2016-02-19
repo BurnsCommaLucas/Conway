490ca84f3e0aad6a627125f98984859669077835
BurnsCommaLucas <lburns534@gmail.com>
2016-02-18 17:37:48 -0800

    Man java graphics are terrible. Woking on them anyway.

--- src/Board.java
+++ src/Board.java
@@ -2,6 +2,7 @@
  * Created by Lucas on 2/18/16.
  */
 import java.util.Random;
+import java.util.Date;
 
 public class Board {
 
@@ -64,7 +65,18 @@ public class Board {
      */
     public void seed ()
     {
-        generator = new Random();
+        Date date = new Date();
+        generator = new Random(date.getTime());
+
+        for (int i = 0; i < dim; i ++)
+        {
+            for (int j = 0; j < dim; j++)
+            {
+                grid[i][j] = generator.nextBoolean();
+            }
+        }
+
+        seedComplete = true;
     }
 
     /**
@@ -72,8 +84,125 @@ public class Board {
      *
      * @param seed String to input random number
      */
-    public void seed (String seed)
+    public void seed (int seed)
     {
+        generator = new Random((long) seed);
 
+        for (int i = 0; i < dim; i ++)
+        {
+            for (int j = 0; j < dim; j++)
+            {
+                grid[i][j] = generator.nextBoolean();
+            }
+        }
+
+        seedComplete = true;
+    }
+
+    /**
+     * getDim - Method to find the dimensions of the board
+     *
+     * @return Int dimension of square board
+     */
+    public int getDim ()
+    {
+        return this.dim;
+    }
+
+    public boolean isAlive (int x, int y)
+    {
+        return this.grid[x][y];
+    }
+
+    public void printBoard ()
+    {
+        for (int i = 0; i < dim; i ++)
+        {
+            for (int j = 0; j < dim; j++)
+            {
+                if (this.grid[i][j])
+                {
+                    System.out.print('0');
+                }
+                else
+                {
+                    System.out.print('1');
+                }
+            }
+            System.out.print('\n');
+        }
+    }
+
+    private int numAdj(int x, int y)
+    {
+        int count = 0;
+
+        for (int dx = -1; dx <= 1; dx++)
+        {
+            for (int dy = -1; dy <= 1; dy++)
+            {
+                if (x + dx >= 0 && x + dx < this.getDim() &&
+                        y + dy >= 0 && y + dy < this.getDim())
+                {
+                    if (this.isAlive(x + dx, y + dy))
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public void advance ()
+    {
+        boolean[][] newGrid = this.grid;
+
+        for (int x = 0; x < this.dim; x++)
+        {
+            for (int y = 0; y < this.dim; y++)
+            {
+                int adj = this.numAdj(x, y);
+
+                if (this.isAlive(x, y))
+                {
+                    if (adj < 2 || adj > 3)
+                    {
+                        newGrid[x][y] = false;
+                    }
+                }
+                else
+                {
+                    if (adj == 3)
+                    {
+                        newGrid[x][y] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    private void nextGeneration (boolean[][] newGrid)
+    {
+        this.grid = newGrid;
+    }
+
+    public int totalLiving ()
+    {
+        int count = 0;
+
+        for (int x = 0; x < this.dim; x++)
+        {
+            for (int y = 0; y < this.dim; y++)
+            {
+                if (this.isAlive(x, y))
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 }
--- src/Conway.java
+++ src/Conway.java
@@ -1,20 +1,193 @@
-import javax.swing.JFrame;
-import javax.swing.WindowConstants;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.*;
+import java.awt.*;
+import javax.swing.*;
+import java.lang.Integer;
+
+import static java.lang.Thread.sleep;
 
 public class Conway
 {
-    private
+    public static Canvas myCanvas = null;
+    public static BufferStrategy strategy = null;
 
-    public static void main(String args[])
+    public static final int MAX_GAME_SIZE = 20;
+    public static final int WINDOW_WIDTH = 600;
+    public static final int WINDOW_HEIGHT = 600;
+    public static boolean isRunning = false;
+    public static int dimIn = 1;
+
+    public static void main (String[] args)
     {
-        JFrame myFrame = new JFrame("Sample Frame");
-        myFrame.setSize(300,400);
-        myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        myFrame.setVisible(true);
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 
-    private void init()
+    public Conway(Canvas initCanvas)
     {
+        myCanvas = initCanvas;
+    }
 
+    public static void repaint() {
+        //Retrieve the canvas used for double buffering
+        Graphics hiddenCanvas = strategy.getDrawGraphics();
+
+        //Start with a black pen on a white background
+        hiddenCanvas.setColor(Color.white);
+        hiddenCanvas.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+        hiddenCanvas.setColor(Color.black);
+
+        //Draw the next frame in the game animation
+        paint(hiddenCanvas);
+
+        //Display the new canvas to the user
+        strategy.show();
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException ie) {
+            //it's safe to ignore this
+        }
+    }
+
+    public static void paint (Graphics canvas)
+    {
+        canvas.setColor(Color.black);
+        canvas.drawRect(10, 10, 100, 100);
+    }
+
+    public static void setup()
+    {
+        JFrame frame = new JFrame("Conway's Game of Life");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        Canvas newCanvas = new Canvas();
+        frame.getContentPane().add(newCanvas);
+
+        Conway newGame = new Conway(newCanvas);
+        frame.setVisible(true);
+
+        newCanvas.createBufferStrategy(2);
+        strategy = newCanvas.getBufferStrategy();
+    }
+
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Conway's Game of Life");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Set up the content pane.
+        addComponentsToPane(frame.getContentPane());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void addComponentsToPane(Container pane)
+    {
+        pane.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        pane.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+
+        JButton startButton = new JButton("Start Game");
+        startButton.setEnabled(false);
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setup();
+            }
+        });
+        c.weightx = 0.5;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        pane.add(startButton, c);
+
+        JTextField text = new JTextField();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 1;
+        c.gridy = 1;
+        pane.add(text, c);
+
+        JButton setButton = new JButton("Set Dimensions");
+        setButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String getText = text.getText();
+                if (getText.matches("\\d+"))
+                {
+                    int input = Integer.parseInt(getText);
+                    if (input < 1 || input > MAX_GAME_SIZE)
+                    {
+                        displayErrorWindow("Please enter an integer from 1 to 20.");
+                        return;
+                    }
+
+                    dimIn = input;
+                    startButton.setEnabled(true);
+                }
+                else
+                {
+                    displayErrorWindow("Please enter an integer from 1 to 20.");
+                }
+            }
+        });
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 1;
+        c.gridy = 0;
+        pane.add(setButton, c);
+
+        JButton pauseButton = new JButton("Pause");
+        pauseButton.setEnabled(false);
+        pauseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (isRunning)
+                {
+                    pauseButton.setText("Resume");
+                    isRunning = false;
+                }
+                else
+                {
+                    pauseButton.setText("Pause");
+                    isRunning = true;
+                }
+            }
+        });
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 2;
+        c.gridy = 0;
+        pane.add(pauseButton, c);
+    }
+
+    public static void displayErrorWindow (String message)
+    {
+        int messageType = JOptionPane.PLAIN_MESSAGE;
+        JOptionPane.showMessageDialog(null, message, "Error", messageType);
+    }
+
+    /**
+     * startGame - Initializes board with user input and starts game
+     *
+     */
+    static private void startGame ()
+    {
+        Board board = new Board(5);
+        int inputSeed = 0;
+        // Prompt for seed
+
+        // If user input a seed then call board.seed(inputSeed);
+
+        // For now just do this
+        board.seed();
     }
 }
